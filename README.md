083283ef1a60a9e1f447aebdb606c70f3b7c4f89
marumemomo <marumemomo@gmail.com>
2018-04-11 22:06:53 +0900

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Todofuken.swift
-Japan Prefecture (都道府県) value for Swift (iOS)
+Japanese Prefecture (都道府県) value for Swift (iOS)
+
+```swift
+let tokyo = Todofuken.tokyo
+print(tokyo.name) // 東京
+print(tokyo.fullName) // 東京都
+```
+
+## Requirements
+* Swift 4.0 or later
+* iOS 9.3 or later
+
+## Installation
+### Carthage
+```
+github "marumemomo/Todofuken.swift"
+```
---
+++ Todofuken/Todofuken.h
@@ -0,0 +1,19 @@
+//
+//  Todofuken.h
+//  Todofuken
+//
+//  Created by marumemomo on 2018/04/11.
+//  Copyright © 2018年 marumemomo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Todofuken.
+FOUNDATION_EXPORT double TodofukenVersionNumber;
+
+//! Project version string for Todofuken.
+FOUNDATION_EXPORT const unsigned char TodofukenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Todofuken/PublicHeader.h>
+
+
